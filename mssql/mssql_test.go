e7bbb0b0bee7616e59178fbf93cfc446319d4401
raystone <raystone1998@outlook.com>
2019-01-15 11:59:40 -0800

    fix test

--- mssql/mssql_test.go
+++ mssql/mssql_test.go
@@ -14,12 +14,11 @@ func InvalidServer(t *testing.T) {
 
 func TestGetTrustedConnectionString(t *testing.T) {
 
-	ok1 := t.Run("valid connection string", func(t *testing.T) {
+	t.Run("valid connection string", func(t *testing.T) {
 		got := GetTrustedConnectionString("a", "b", 0)
 		want := "server=a;database=b;Trusted_Connection=True;"
 		assertCorrectMessage(t, got, want)
 	})
-	assertTrue(t, ok1)
 }
 
 func TestInvalidServer(t *testing.T) {
@@ -37,17 +36,6 @@ func TestInvalidServer(t *testing.T) {
 	t.Fatalf("process rn with err %v, want exit status 1", err)
 }
 
-func assertFalse(t *testing.T, ok bool) {
-	if ok == true {
-		t.Errorf("ok was %v", ok)
-	}
-}
-
-func assertTrue(t *testing.T, ok bool) {
-	if ok == false {
-		t.Errorf("ok was %v", ok)
-	}
-}
 func assertCorrectMessage(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
